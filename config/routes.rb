309f003e2f98aa16b3f80d4baebfc47f047cefdd
jmelo <jmeloa@gmail.com>
2020-09-19 10:24:33 -0300

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :stories
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:create, :destroy, :new]
   root 'stories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
